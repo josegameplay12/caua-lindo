a6dc7d657467081648902c22fd3f82d68e167bdf
josegameplay12 <116831491+josegameplay12@users.noreply.github.com>
2024-10-29 08:26:10 -0300

    dsadas

--- projeto2.js
+++ projeto2.js
@@ -19,11 +19,8 @@ function criaCartao(categoria, pergunta, resposta){
     function mostraresposta(card) {
 const resposta = card.querySelector('.resposta-cartao');
 if(resposta.style.display === 'none') {
-resposta.style.display === 'block';
-
+resposta.style.display = 'block';
     }
     else {
         resposta.style.display === 'none';
     }
-    
-    
